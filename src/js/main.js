5d65102d0f3311afe9d679eacfb8f7c624b2d66f
patlau <patrick.lauper@less.ch>
2016-09-19 19:03:13 +0200

    Read Notes from JSON

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,13 @@
 
 (function() {
 
+    /*function Note(title, description, importance, due, done) {
+        this.description = description;
+        this.importance = importance;
+        this.due = due;
+        this.done = done;
+    }*/
+
     function getAbout() {
         return {
             project: 'HSR FEE Projekt 1',
@@ -17,13 +24,33 @@
     }
 
     function loadTemplates() {
-        loadTemplate("header", "#header-template", {});
-        loadTemplate("main", "#list-template", {});
-        loadTemplate("footer", "#footer-template", getAbout());
+//        loadTemplate("header", "#header-template", {});
+        loadTemplate("main", "#master-template", {"notes": []});
+        loadTemplate(".app-footer", "#app-footer-template", getAbout());
+    }
+
+    function loadNotes() {
+        console.log("Load notes");
+        $.getJSON( "test-data/notes.json")
+            .done(function( data ) {
+                console.log(data);
+                //for (let item of data) {
+                //    let note = $.extend(new Note(), item);
+                //    console.log(note);
+                //}
+                loadTemplate("main", "#master-template", {"notes": data});
+            })
+            .fail(function( data ) {
+                console.log( "error: " + data );
+            })
+            .always(function() {
+                console.log( "complete" );
+            });
     }
 
     $(document).ready(function () {
         loadTemplates();
+        loadNotes();
     });
 
 })();
