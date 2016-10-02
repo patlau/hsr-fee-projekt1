4c77ed8947f3de82e6eba5031c958c9d22ba16b4
Travis CI <plauper@yahoo.com>
2016-10-02 10:50:30 +0000

    Deploy to GitHub Pages: e9b0c75424808de8704cfbec8e4d4f3a806516e6

--- js/about.js
+++ js/about.js
@@ -15,7 +15,7 @@ var aboutModule = (function(){
         build: 'localbuild',
         commit: '',
         branch: 'master',
-        datetime: '2016-10-02T10:18:09.670Z'
+        datetime: '2016-10-02T10:50:29.572Z'
     };
 
     return {
