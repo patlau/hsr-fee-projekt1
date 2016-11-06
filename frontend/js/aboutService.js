602d090a0bdeadae7ceb423adbe58d6c262f921f
Travis CI <travis@build.local>
2016-11-06 12:30:27 +0000

    Deploy to GitHub Pages: 639071469646b9cdb5addbd484cba040281366e1

--- frontend/js/aboutService.js
+++ frontend/js/aboutService.js
@@ -15,7 +15,7 @@ var aboutModule = (function(){
         build: 'localbuild',
         commit: '',
         branch: 'master',
-        datetime: '2016-11-06T12:19:16.199Z'
+        datetime: '2016-11-06T12:30:26.606Z'
     };
 
     return {
